23350bd4461a791326b1c5c5b759c9f02778ba76
Rishav Kumar <rishav74492644@gmail.com>
2024-04-13 17:05:52 +0530

    Create new card UI.

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,26 +1,24 @@
-import Footer from "./footer";
 import Header from "./header";
-
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import Footer from "./footer";
+import { tools } from "./tools";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col items-start h-screen w-full bg-gray-200">
+    <div className="flex flex-col items-start h-screen w-full bg-white">
       <Header />
-      <div className="h-full w-full overflow-auto py-6 mb-6 bg-gray-200">
-        <div className="grid grid-cols-2 grid-flow-row gap-12 px-12 bg-gray-200">
-          {array.map((a, index) => (
+      <div className="h-full w-full overflow-auto">
+        <div className="grid grid-cols-3 grid-flow-row gap-24 p-12">
+          {tools.map((tool) => (
             <div
-              className="relative w-full h-auto bg-slate-50 p-4 rounded-md transition duration-500 ease-out border-2 border-blue-200 card"
-              key={index}
+              className="w-1/1 h-64 rounded-lg text-xl cursor-pointer card"
+              key={tool.id}
             >
-              <div className="flex flex-col">
-                <p className="text-xl">Card title - {a}</p>
-                <p className="">Here are the details of the card</p>
+              <div className="h-full w-full flex items-center justify-center first-content">
+                <span>{tool.label}</span>
               </div>
-              <button className="absolute w-36 rounded-lg bg-blue-500 text-white py-2 left-1/2 bottom-0 transition duration-300 ease-out opacity-0 card-button">
-                More info
-              </button>
+              <div className="opacity-0 flex items-center justify-center second-content">
+                {tool.description}
+              </div>
             </div>
           ))}
         </div>
